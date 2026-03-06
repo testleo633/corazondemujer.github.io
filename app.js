539eb9e2e65fdcaabc70c9b2d4906b365e30fc1e
testleo633 <testleo633@gmail.com>
2026-03-05 23:28:18 -0700

    Initialize Supabase client in app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://kpjpkedoyudhvhqoxvov.supabase.co"
+
+const SUPABASE_KEY = "sb_publishable_VLKr4SZgPn-epvzGourmkA_iVWd1aPI"
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+)
