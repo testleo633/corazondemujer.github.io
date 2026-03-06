21bb8c5bd495e2a99850d2061119a9ad5e9eba18
testleo633 <testleo633@gmail.com>
2026-03-05 23:46:16 -0700

    Fix SUPABASE_URL typo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://kpjpkedoyudhvhqoxvov.supabase.co"
+const SUPABASE_URL = "https://kpjpkedoyudhvhoqxvov.supabase.co"
 
 const SUPABASE_KEY = "sb_publishable_VLKr4SZgPn-epvzGourmkA_iVWd1aPI"
 
